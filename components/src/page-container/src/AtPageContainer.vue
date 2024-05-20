fab975c954af664991f674ae1b697b1aa2015a74
沈孟平 <artsmp@126.com>
2024-05-20 16:16:20 +0800

    feat(AtPageContainer): update

--- components/src/page-container/src/AtPageContainer.vue
+++ components/src/page-container/src/AtPageContainer.vue
@@ -15,7 +15,7 @@ const props = withDefaults(
   }>(),
   { padding: '16px' },
 )
-const styles = computed(() => ({
+const style = computed(() => ({
   padding: `${Number.parseInt(props.padding as string)}px`,
   paddingTop: `${Number.parseInt(props.padding as string)}px`,
 }))
@@ -24,9 +24,9 @@ const $wrapper = shallowRef<HTMLElement>()
 </script>
 
 <template>
-  <div ref="$wrapper" class="h-full w-full" :class="[innerScroll ? 'flex flex-col of-hidden' : 'of-auto']" :style="styles">
+  <div ref="$wrapper" class="h-full w-full" :class="[innerScroll ? 'flex flex-col of-hidden' : 'of-auto']" :style="innerScroll ? style : undefined">
     <NScrollbar v-if="!innerScroll" :x-scrollable="xScrollable">
-      <div :style="styles">
+      <div :style>
         <slot />
       </div>
       <NBackTop v-if="backTop" class="z-1000" right="40" :to="$wrapper" />
